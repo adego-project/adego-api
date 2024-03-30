9d725152d3843a79c887c78b4754cca615efdc89
suk-6 <me@suk.kr>
2024-03-30 11:05:18 +0900

    Add PrismaModule and AuthModule imports to GoogleModule

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GoogleController } from './google.controller';
 import { GoogleService } from './google.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+    imports: [PrismaModule, AuthModule],
     controllers: [GoogleController],
     providers: [GoogleService],
 })
