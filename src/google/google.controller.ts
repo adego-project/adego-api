533c33709c1e522bf79a3ddab825b5cb3e60971b
suk-6 <me@suk.kr>
2024-03-30 11:09:17 +0900

    Add Swagger annotations and GoogleService dependency to GoogleController

--- src/google/google.controller.ts
+++ src/google/google.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { OAuthTokenDTO } from 'src/dto';
+import { GoogleService } from './google.service';
+import { ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
+@ApiTags('oauth')
 @Controller('/oauth/google')
 export class GoogleController {
-    constructor() {}
+    constructor(private readonly googleService: GoogleService) {}
 
     @Post('login')
+    // Swagger
+    @ApiOperation({ summary: 'Google oAuth login endpoint' })
+    @ApiOkResponse({ description: 'Google oAuth login success' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized (구글 사용자 정보 조회 실패)' })
     async login(@Body() dto: OAuthTokenDTO) {
-        console.log(dto);
+        return this.googleService.login(dto);
     }
 }
