07d5a9e19de27babe3d75a5113ebec134a5c606f
suk-6 <me@suk.kr>
2024-07-11 23:08:40 +0900

    feat: Add AppleJwtTokenPayload interface for Apple user model

---
+++ src/model/apple.user.model.ts
@@ -0,0 +1,14 @@
+export interface AppleJwtTokenPayload {
+    iss: string;
+    aud: string;
+    exp: number;
+    iat: number;
+    sub: string;
+    nonce: string;
+    c_hash: string;
+    email?: string;
+    email_verified?: string;
+    is_private_email?: string;
+    auth_time: number;
+    nonce_supported: boolean;
+}
