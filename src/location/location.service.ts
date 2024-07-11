3dd61acc4b23ccdeeca18ebbbeb67e1d32adfea3
suk-6 <me@suk.kr>
2024-07-12 05:04:37 +0900

    Feat: location service

---
+++ src/location/location.service.ts
@@ -0,0 +1,16 @@
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { UpdateLocationDTO } from './dto/update.dto';
+
+@Injectable()
+export class LocationService {
+    constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+    async updateLocationById(id: string, data: UpdateLocationDTO) {
+        return this.cacheManager.set(id, data);
+    }
+
+    async getLocationById(id: string) {
+        return this.cacheManager.get(id);
+    }
+}
