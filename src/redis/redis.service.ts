2eb60222b165eb2a3ec1ce5762cf91079f0d6b90
suk-6 <me@suk.kr>
2024-07-12 05:04:46 +0900

    feat: Update Redis service configuration to use environment variables

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CacheModuleOptions, CacheOptionsFactory } from '@nestjs/cache-manager';
 import redisStore from 'cache-manager-redis-store';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RedisService implements CacheOptionsFactory {
+    constructor(private readonly configService: ConfigService) {}
     createCacheOptions(): CacheModuleOptions {
         const config: CacheModuleOptions = {
             store: redisStore,
-            host: 'localhost',
-            port: 6379,
+            host: this.configService.get<string>('REDIS_HOST'),
+            port: this.configService.get<string>('REDIS_PORT'),
         };
         return config;
     }
