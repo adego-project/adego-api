c01c4b7c3884401acf4029288459ec8bf4f94554
PleBea <bini060315kim@gmail.com>
2024-07-12 08:43:13 +0900

    fix: Edit google oauth method

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,15 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { OAuth2Client } from 'google-auth-library';
 import { AuthService } from 'src/auth/auth.service';
 import { OAuthTokenDTO } from 'src/dto';
-import { AuthType, GoogleUser } from 'src/model';
+import { AuthType } from 'src/model';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class GoogleService {
+    private readonly oAuth2Client: OAuth2Client;
+
     constructor(
         private readonly prisma: PrismaService,
         private readonly authService: AuthService,
-    ) {}
+    ) {
+        this.oAuth2Client = new OAuth2Client();
+    }
 
     async login(dto: OAuthTokenDTO) {
         const googleUser = await this.getGoogleUser(dto.accessToken);
@@ -30,14 +35,20 @@ export class GoogleService {
     }
 
     async getGoogleUser(accessToken: string) {
-        const user: GoogleUser = await fetch(
-            `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`,
-        ).then((res) => (res.status === 200 ? res.json() : null));
+        // const user: GoogleUser = await fetch(
+        //     `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`,
+        // ).then((res) => (res.status === 200 ? res.json() : null));
+        const ticket = await this.oAuth2Client.verifyIdToken({
+            idToken: accessToken,
+        });
+        const payload = ticket.getPayload();
 
-        if (!user) {
+        if (!payload) {
             throw new UnauthorizedException('A03'); // A03 - 구글 사용자 정보 조회 실패
         }
 
-        return user;
+        console.log(payload);
+
+        return payload;
     }
 }
