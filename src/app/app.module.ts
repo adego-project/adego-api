9e39e5af5eb11097e146ddce62a21803bd1adc6b
PleBea <bini060315kim@gmail.com>
2024-07-12 22:57:21 +0900

    refactor: Update imports and module structure for cache and config modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,16 @@
-import { redisStore } from 'cache-manager-redis-yet';
 import importToArray from 'import-to-array';
-import { RedisClientOptions } from 'redis';
 
-import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { validate } from 'src/common';
 import * as commonModules from 'src/common/modules';
+import * as providers from 'src/common/providers';
 import * as modules from 'src/modules';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: `.env.${process.env.NODE_ENV}`,
-            validate,
-            validationOptions: {
-                abortEarly: true,
-            },
-        }),
-        CacheModule.registerAsync<RedisClientOptions>({
-            imports: [ConfigModule],
-            isGlobal: true,
-            useFactory: async (config: ConfigService) => ({
-                store: await redisStore({
-                    socket: {
-                        host: config.get('REDIS_HOST'),
-                        port: config.get('REDIS_PORT'),
-                    },
-                }),
-            }),
-            inject: [ConfigService],
-        }),
-        ...importToArray(modules),
-        ...importToArray(commonModules),
-    ],
+    imports: [...importToArray(providers), ...importToArray(modules), ...importToArray(commonModules)],
     providers: [AppService],
     controllers: [AppController],
 })
