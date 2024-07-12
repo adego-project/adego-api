40b26d8e583c813ec1b027cdb02922bd4d62e356
suk-6 <me@suk.kr>
2024-07-12 23:26:46 +0900

    refactor: Update LocationService to include getParticipantsLocation method

--- src/modules/location/location.service.ts
+++ src/modules/location/location.service.ts
@@ -1,17 +1,37 @@
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 
-import { UpdateLocationDTO } from './dto/update.dto';
+import { LocationDTO } from './dto/update.dto';
+import { PlanService } from '../plan/plan.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class LocationService {
-    constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+    constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache, private readonly planService: PlanService) {}
 
-    async updateLocationById(id: string, data: UpdateLocationDTO) {
+    async updateLocationById(id: string, data: LocationDTO) {
         return this.cacheManager.set(id, data);
     }
 
-    async getLocationById(id: string) {
+    /*
+    * Get participants location
+    * @param User
+    * @returns participants location {
+    *   id: UpdateLocationDTO
+    * }
+    */
+    async getParticipantsLocation(user: User): Promise<Record<string, LocationDTO>> {
+        const plan = await this.planService.getPlan(user);
+
+        const locations = {};
+        plan.users.map(async (user) => 
+            locations[user.id] = await this.getLocationById(user.id)
+        );
+
+        return locations;
+    }
+
+    async getLocationById(id: string): Promise<LocationDTO | undefined> {
         return this.cacheManager.get(id);
     }
 }
