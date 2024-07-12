7858508cd60945861131ce324707399bc68ac88b
suk-6 <me@suk.kr>
2024-07-12 23:26:51 +0900

    refactor: Update LocationController to include getParticipantsLocation endpoint

--- src/modules/location/location.module.ts
+++ src/modules/location/location.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
+import { PlanModule } from '../plan';
 
 @Module({
+    imports: [PlanModule],
     controllers: [LocationController],
     providers: [LocationService],
 })
