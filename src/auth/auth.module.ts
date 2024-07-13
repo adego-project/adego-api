4cf2fa478f5013e7a543c9e1cea13f7ed5e8071f
suk-6 <me@suk.kr>
2024-07-13 18:00:26 +0900

    refactor: Add PrismaModule to imports in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { PrismaModule } from 'src/common';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessJwtStrategy } from './strategy/accessToken.strategy';
 import { RefreshJwtStrategy } from './strategy/refreshToken.strategy';
 
 @Module({
-    imports: [JwtModule.register({}), PassportModule],
+    imports: [JwtModule.register({}), PassportModule, PrismaModule],
     providers: [AuthService, RefreshJwtStrategy, AccessJwtStrategy],
     controllers: [AuthController],
     exports: [AuthService],
