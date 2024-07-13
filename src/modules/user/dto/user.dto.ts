2114bcf347542d2fb923a1e290ac482afee4c5c7
suk-6 <me@suk.kr>
2024-07-13 17:45:47 +0900

    refactor: Remove unused properties from UserResponseDTO

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -2,15 +2,9 @@ import { User } from '@prisma/client';
 
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UserResponseDTO implements User {
+export class UserResponseDTO implements Partial<User> {
     @ApiProperty()
     id: string;
-
-    @ApiProperty()
-    provider: string;
-
-    @ApiProperty()
-    providerId: string;
 
     @ApiProperty()
     name: string;
