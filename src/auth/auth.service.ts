abe1e61f831c1bbbc7f61afab193a47e1a9cc210
suk-6 <me@suk.kr>
2024-07-13 18:00:29 +0900

    refactor: Add createUser method to AuthService for creating a new user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,46 @@
+import { randomUUID } from 'crypto';
+
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
-import { TokenPayload } from 'src/common';
+import { AuthType, TokenPayload } from 'src/common';
+import { PrismaService } from 'src/common/modules/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
     constructor(
+        private readonly prisma: PrismaService,
         private readonly jwtService: JwtService,
         private readonly configService: ConfigService,
     ) {}
+
+    async createUser(provider: AuthType, providerId: string) {
+        return await this.prisma.user.create({
+            data: {
+                id: randomUUID(),
+                provider,
+                providerId,
+            },
+        });
+    }
+
+    async findUserById(id: string) {
+        return this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+
+    async findUserByProvider(provider: AuthType, providerId: string) {
+        return this.prisma.user.findFirst({
+            where: {
+                provider,
+                providerId,
+            },
+        });
+    }
 
     async createTokens(id: string) {
         const accessToken = await this.createAccessToken(id);
