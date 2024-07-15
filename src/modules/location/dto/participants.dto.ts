9b9b7c8f20602991e1809ebcefa83216b92a5c27
suk-6 <me@suk.kr>
2024-07-15 10:00:13 +0900

    refactor: Update ParticipantsDTO example and type in participants.dto.ts

--- src/modules/location/dto/participants.dto.ts
+++ src/modules/location/dto/participants.dto.ts
@@ -6,14 +6,12 @@ import { LocationDTO } from './update.dto';
 
 export class ParticipantsDTO {
     @ApiProperty({
-        example: [
-            {
-                lat: '33.511111',
-                lng: '126.492778',
-            },
-        ],
+        example: {
+            lat: '33.511111',
+            lng: '126.492778',
+        },
         type: [LocationDTO],
     })
     @IsArray()
-    'userid': LocationDTO[];
+    'userid': LocationDTO;
 }
