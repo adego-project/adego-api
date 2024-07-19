a8d6c0df1cb566174aff1294af527a83eb0a1ce5
suk-6 <me@suk.kr>
2024-07-19 13:29:26 +0900

    refactor: Add validation to UpdateUserFCMTokenDTO for FCMToken field

--- src/modules/user/dto/update-user-fcm.dto.ts
+++ src/modules/user/dto/update-user-fcm.dto.ts
@@ -1,12 +1,14 @@
 import { Prisma } from '@prisma/client';
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserFCMTokenDTO implements Prisma.UserUpdateInput {
     @ApiProperty({
         required: true,
+        maxLength: 200,
     })
     @IsString()
+    @MaxLength(200)
     FCMToken: string;
 }
